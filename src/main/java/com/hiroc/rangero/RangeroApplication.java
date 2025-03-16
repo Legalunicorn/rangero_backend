1a6fdbfe9739a4db756818a3c075f1f70af3da88
Nathan Yeo <nyeo700@gmail.com>
2025-03-16 16:15:14 +0800

    init

---
+++ src/main/java/com/hiroc/rangero/RangeroApplication.java
@@ -0,0 +1,13 @@
+package com.hiroc.rangero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangeroApplication.class, args);
+	}
+
+}
